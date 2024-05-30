0ffc8816ecaebf4fdce2b47e23395044684bf23f
Pritesh1509 <41056930+Pritesh1509@users.noreply.github.com>
2024-05-30 09:41:14 +0530

    Update archive-resource.php

--- templates/archive-resource.php
+++ templates/archive-resource.php
@@ -1,20 +1,20 @@
 <?php get_header(); 
 
-$resource_type = get_terms( 'resource_type', array( 'hide_empty' => false) );
-$resource_topics = get_terms( 'resource_topic', array( 'hide_empty' => false) ); 
+$resource_type = get_terms( array('taxonomy'=>'resource_type','hide_empty' => false) );
+$resource_topics = get_terms(array('taxonomy'=>'resource_topic','hide_empty' => false) ); 
 ?>
 <div class="container">
     <div class="resource-container">
         <select name="resource_filter" id="r_filter">
             <option name="">Select Resource Types</option>
                 <?php foreach($resource_type as $type){ ?>
-                    <option name="<?php echo $type->slug; ?>"><?php echo $type->name; ?></option>
+                    <option value="<?php echo $type->slug; ?>"><?php echo $type->name; ?></option>
                 <?php } ?>
         </select>
         <select name="resource_topic_filter" id="t_filter">
             <option name="">Select Resource Topics</option>
                 <?php foreach($resource_topics as $topic){ ?>
-                    <option name="<?php echo $topic->slug; ?>"><?php echo $topic->name; ?></option>
+                    <option value="<?php echo $topic->slug; ?>"><?php echo $topic->name; ?></option>
                 <?php } ?>
         </select>
     </div>
@@ -29,9 +29,9 @@ $resource_topics = get_terms( 'resource_topic', array( 'hide_empty' => false) );
         $query = new WP_Query($args);
         if($query->have_posts()){
             while($query->have_posts()){ 
-            $query->the_post();
-            the_title(); echo '<br>';
-            }
+            $query->the_post(); ?>
+            <div class="post-title"><h3><?php the_title(); ?></h3></div>
+            <?php }
         } ?>
     </div>
     <div id="response_data"></div>
