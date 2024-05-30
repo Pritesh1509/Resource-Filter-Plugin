b6720fbee6a50a4301e0fdcd077824fa1f3d81b5
Pritesh1509 <41056930+Pritesh1509@users.noreply.github.com>
2024-05-30 08:26:25 +0530

    Update customjs.js

--- js/customjs.js
+++ js/customjs.js
@@ -2,8 +2,6 @@ jQuery(document).ready(function(){
     function resource_filter_data(){
         var resource_type = jQuery('#r_filter').val();
         var resource_topic = jQuery('#t_filter').val();
-        var search = jQuery('#search').val();
-
         jQuery.ajax({
             url:my_object.ajax_url,
             type: 'POST',
@@ -11,13 +9,11 @@ jQuery(document).ready(function(){
                 action:'resource_data',
                 resource_type:resource_type,
                 resource_topic:resource_topic,
-                search:search,
             },
             success: function(data) {
                 jQuery('#resource_listing').hide();
                 jQuery('#response_data').html(data); // insert data
             }
-
         });
     }
    
@@ -28,9 +24,4 @@ jQuery(document).ready(function(){
     jQuery('#t_filter').change(function(){
         resource_filter_data();
     });
-
-    /*jQuery('#search').on('keyup change', function() {
-        resource_filter_data();
-    });*/
-
 });
